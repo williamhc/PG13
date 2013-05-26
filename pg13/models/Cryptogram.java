c9819731ce3ee81d8d7afb9468ffd99fdd99f4e3
Lauren Slusky <laurenslusky@gmail.com>
2013-05-26 13:43:45 -0500

    Basic Creation of class Cryptogram extending puzzle

---
+++ pg13/models/Cryptogram.java
@@ -0,0 +1,85 @@
+package pg13.models;
+
+import java.util.Date;
+
+/*
+ *  @author Lauren Slusky
+ *  @date May 26 2013
+ *  @title Cryptogram Class
+ *  @description Class defining puzzle type Cryptogram
+ */
+public class Cryptogram extends Puzzle
+{
+	private String cipherText;	// Coded string
+	private String plainText;	// the original user string
+	
+	
+	/*
+	 *  @author Lauren Slusky
+	 *  @date May 26 2013
+	 *  @title Cryptogram Constructor
+	 */
+	public Cryptogram()
+	{
+		super();
+		this.setCipherText("");
+		this.setPlainText("");
+	}
+	
+	/*
+	 *  @author Lauren Slusky
+	 *  @date May 26 2013
+	 *  @title Cryptogram Constructor
+	 *  @param String author - whoever designed the cryptogram
+	 *  @param String title - name of cryptogram
+	 *  @param Date dateCreated - date of creation of puzzle
+	 *  @param String ct - ciphertext for given plaintext
+	 *  @param String pt - plaintext given by the user for the puzzle
+	 */ 
+	public Cryptogram(String author, String title, Date dateCreated, String ct, String pt)
+	{
+		super(author, title, dateCreated);
+		this.setCipherText(ct);
+		this.setPlainText(pt);
+	}
+
+	/*
+	 *  @author Lauren Slusky
+	 *  @date May 26 2013
+	 *  @title getCipherText
+	 *  @return String cipherText
+	 */
+	public String getCipherText() {
+		return cipherText;
+	}
+
+	/*
+	 *  @author Lauren Slusky
+	 *  @date May 26 2013
+	 *  @title setCipherText
+	 *  @param String cipherText to set for a given puzzle
+	 */
+	public void setCipherText(String cipherText) {
+		this.cipherText = cipherText;
+	}
+
+	/*
+	 *  @author Lauren Slusky
+	 *  @date May 26 2013
+	 *  @title getPlainText
+	 *  @return plaintext for a puzzle
+	 */
+	public String getPlainText() {
+		return plainText;
+	}
+
+	/*
+	 *  @author Lauren Slusky
+	 *  @date May 26 2013
+	 *  @title setPlainText
+	 *  @return sets plaintext for a puzzle
+	 */
+	public void setPlainText(String plainText) {
+		this.plainText = plainText;
+	}
+}

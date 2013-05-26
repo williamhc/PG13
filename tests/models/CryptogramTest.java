683aae8f67c24208a4daaac77e76d93a7146608f
Lauren Slusky <laurenslusky@gmail.com>
2013-05-26 14:51:37 -0500

    Wrote some more tests

--- tests/models/CryptogramTest.java
+++ tests/models/CryptogramTest.java
@@ -25,57 +25,92 @@ import pg13.models.Cryptogram;
 			System.out.println("Finished testCryptogram");
 		}
 		
-		public void testCryptogramData1()
+		public void testCryptogramGeneralData()
 		{
 			Cryptogram cryptogram;
 
-			System.out.println("\nStarting testCryptogramData1");		
+			System.out.println("\nStarting testCryptogramGeneralData");
 			cryptogram = new Cryptogram("Lauren Slusky", "Practice Cryptogram", new Date(), "This is a test");
 			assertNotNull(cryptogram);
 			assertEquals("Lauren Slusky", cryptogram.getAuthor());
 			assertEquals("Practice Cryptogram", cryptogram.getTitle());
+			System.out.println("Finished testCryptogramGeneralData");
+		}
+
+		public void testCipherTextWorks1()
+		{
+			Cryptogram cryptogram;
+
+			System.out.println("\nStarting testCipherTextWorks1");		
+			cryptogram = new Cryptogram("Lauren Slusky", "Practice Cryptogram", new Date(), "This is a test");
 			assertEquals("This is a test", cryptogram.getPlainText());
 			assertNotSame("This is a test", cryptogram.getCipherText());
 			System.out.println(cryptogram.getPlainText());
 			System.out.println(cryptogram.getCipherText());
 			
-			System.out.println("Finished testCryptogramData1");
+			System.out.println("Finished testCipherTextWorks1");
 			
 		}
 		
-		public void testCryptogramData2()
+		public void testCipherTextWorks2()
 		{
 			Cryptogram cryptogram;
 
-			System.out.println("\nStarting testCryptogramData2");		
+			System.out.println("\nStarting testCipherTextWorks2");		
 			cryptogram = new Cryptogram("Lauren Slusky", "Practice Cryptogram", new Date(), "This. is, a! test%");
-			assertNotNull(cryptogram);
-			assertEquals("Lauren Slusky", cryptogram.getAuthor());
-			assertEquals("Practice Cryptogram", cryptogram.getTitle());
 			assertEquals("This. is, a! test%", cryptogram.getPlainText());
 			assertNotSame("This is a test", cryptogram.getCipherText());
 			System.out.println(cryptogram.getPlainText());
 			System.out.println(cryptogram.getCipherText());
 			
-			System.out.println("Finished testCryptogramData2");
+			System.out.println("Finished testCipherTextWorks2");
 			
 		}
 		
-		public void testCryptogramData3()
+		public void testCryptogramEmptyPlainText()
 		{
 			Cryptogram cryptogram;
 
-			System.out.println("\nStarting testCryptogramData3");		
+			System.out.println("\nStarting testCryptogramEmptyPlainText");		
 			cryptogram = new Cryptogram("Lauren Slusky", "Practice Cryptogram", new Date(), "");
-			assertNotNull(cryptogram);
-			assertEquals("Lauren Slusky", cryptogram.getAuthor());
-			assertEquals("Practice Cryptogram", cryptogram.getTitle());
 			assertEquals("", cryptogram.getPlainText());
 			assertEquals("", cryptogram.getCipherText());
 			System.out.println(cryptogram.getPlainText());
 			System.out.println(cryptogram.getCipherText());
 			
-			System.out.println("Finished testCryptogramData3");
+			System.out.println("Finished testCryptogramEmptyPlainText");
+			
+		}
+		
+		public void testCryptogramEmptyAuthor()
+		{
+			Cryptogram cryptogram;
+
+			System.out.println("\nStarting testCryptogramEmptyAuthor");		
+			cryptogram = new Cryptogram("", "Practice Cryptogram", new Date(), "");
+			assertEquals("", cryptogram.getAuthor());			
+			System.out.println("Finished testCryptogramEmptyAuthor");
+			
+		}
+		
+		public void testCryptogramEmptyTitle()
+		{
+			Cryptogram cryptogram;
+
+			System.out.println("\nStarting testCryptogramEmptyTitle");		
+			cryptogram = new Cryptogram("Lauren Slusky", "", new Date(), "");
+			assertEquals("", cryptogram.getTitle());			
+			System.out.println("Finished testCryptogramEmptyTitle");
+			
+		}
+		public void testCryptogramEmptyDate()
+		{
+			Cryptogram cryptogram;
+
+			System.out.println("\nStarting testCryptogramEmptyDate");		
+			cryptogram = new Cryptogram("Lauren Slusky", "Practice Cryptogram", null, "");
+			assertEquals(null, cryptogram.getDateCreated());
+			System.out.println("Finished testCryptogramEmptyDate");
 			
 		}
 	}

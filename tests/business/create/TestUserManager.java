085b21af64ce3cd79484a09190add596fc28aca1
Paymahn Moghadasian <paymahn1@gmail.com>
2013-06-20 21:55:57 -0500

    Testing completed?
    I think I finished testing for the user manager. I can't think of more
    things that can be done.

--- tests/business/create/TestUserManager.java
+++ tests/business/create/TestUserManager.java
@@ -17,20 +17,34 @@ public class TestUserManager extends TestCase
 		assertNotNull(manager.findUser(UserController.getGuestPrimaryKey()));
 		assertNull(manager.findUser(UserController.getGuestPrimaryKey() + 1));
 		assertNull(manager.findUser(UserController.getGuestPrimaryKey() - 1));
+		assertEquals(1, manager.getNameOfAllUsers().size());
 		this.checkForUnwantedUsers();
 		
 		User user1 = manager.addUser(Constants.AUTHOR);
 		
 		assertNotNull(manager.findUser(UserController.getGuestPrimaryKey()));
 		assertEquals(user1, manager.findUser(user1));
+		assertEquals(2, manager.getNameOfAllUsers().size());
 		this.checkForUnwantedUsers();
 		
 		User user2 = manager.addUser("Another author");
 		assertNotNull(manager.findUser(UserController.getGuestPrimaryKey()));
 		assertEquals(user1, manager.findUser(user1));
 		assertEquals(user2, manager.findUser(user2));
+		assertEquals(3, manager.getNameOfAllUsers().size());
 		this.checkForUnwantedUsers();
+		
+		assertEquals(Constants.AUTHOR, manager.getNameOfUser(user1.getPrimaryKey()));
+		assertEquals("Another author", manager.getNameOfUser(user2.getPrimaryKey()));
 	
+		//test adding person with same name
+		User user3 = manager.addUser(Constants.AUTHOR);
+		assertNotNull(manager.findUser(UserController.getGuestPrimaryKey()));
+		assertEquals(user1, manager.findUser(user1));
+		assertEquals(user2, manager.findUser(user2));
+		assertEquals(user3, manager.findUser(user3));
+		assertEquals(4, manager.getNameOfAllUsers().size());
+		this.checkForUnwantedUsers();
 	}
 	
 	private void checkForUnwantedUsers()

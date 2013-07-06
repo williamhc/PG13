ab650d5e5932f7e7d30b9e45a11c426db899ee37
Lauren Slusky <laurenslusky@gmail.com>
2013-07-06 17:08:36 -0500

    stuff for eric about the injection thing he wrote

--- src/tests/atrStubs/MessageBoxMakerStub.java
+++ src/tests/atrStubs/MessageBoxMakerStub.java
@@ -9,30 +9,28 @@ import acceptanceTests.Register;
 
 import pg13.presentation.IMessageBoxStrategy;
 
-public class MessageBoxMakerStub extends Composite implements IMessageBoxStrategy
+public class MessageBoxMakerStub implements IMessageBoxStrategy
 {
-	Label lblMessage;
+	private Label lblMessage;
 	
 	public MessageBoxMakerStub()
 	{
-		super(null, SWT.NONE);
-		
-		lblMessage = new Label(this, SWT.NONE);
-		
-		Register.newWindow(this);
+		//super(null, SWT.NONE);
+		//this.lblMessage = new Label(null, SWT.NONE);	
+		Register.newWindow(this);		
 	}
 
 	@Override
 	public void errorMessage(Shell shell, String header, String message) 
 	{
-		lblMessage.setText(message);
+		//lblMessage.setText(message);
 		
 	}
 
 	@Override
 	public void infoMessage(Shell shell, String header, String message) 
 	{
-		lblMessage.setText(message);
+		//lblMessage.setText(message);
 		
 	}
 

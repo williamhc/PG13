6d61e8a85766cecbf5e622ce0c4df67bfb0a6b52
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2013-07-07 18:05:55 -0500

    renamed test suites

--- src/tests/AllTests.java
+++ src/tests/AllTests.java
@@ -1,21 +1,6 @@
 package tests;
 
-import tests.business.AuthorFilterTest;
-import tests.business.CategoryFilterTest;
-import tests.business.CryptogramManagerTest;
-import tests.business.DifficultyFilterTest;
-import tests.business.PuzzleAuthorColumnProviderTest;
-import tests.business.PuzzleCategoryColumnProviderTest;
-import tests.business.PuzzleDifficultyColumnProviderTest;
-import tests.business.PuzzleManagerTest;
-import tests.business.PuzzleTableDriverTest;
-import tests.business.PuzzleTitleColumnProviderTest;
-import tests.business.TestUserManager;
-import tests.business.TitleFilterTest;
-import tests.models.CryptogramPairTest;
-import tests.models.CryptogramTest;
-import tests.models.UserTest;
-import tests.persistence.TestStubDB;
+import acceptanceTests.TestRunner;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -27,37 +12,9 @@ public class AllTests
 	{
 		suite = new TestSuite("All tests");
 		DBResetter.ResetHSQLDB();
-		testModels();
-		testBusiness();
-		testPersistence();
+		suite.addTest(UnitTests.suite());
+		suite.addTest(IntegrationTests.suite());
+		suite.addTestSuite(AcceptanceTests.class);
 		return suite;
-	}
-
-	private static void testModels()
-	{
-		suite.addTestSuite(CryptogramPairTest.class);
-		suite.addTestSuite(CryptogramTest.class);
-		suite.addTestSuite(UserTest.class);
-	}
-
-	private static void testBusiness()
-	{
-		suite.addTestSuite(CryptogramManagerTest.class);
-		suite.addTestSuite(PuzzleManagerTest.class);
-		suite.addTestSuite(PuzzleAuthorColumnProviderTest.class);
-		suite.addTestSuite(PuzzleCategoryColumnProviderTest.class);
-		suite.addTestSuite(PuzzleDifficultyColumnProviderTest.class);
-		suite.addTestSuite(PuzzleTitleColumnProviderTest.class);
-		suite.addTestSuite(TestUserManager.class);
-		suite.addTestSuite(PuzzleTableDriverTest.class);
-		suite.addTestSuite(TitleFilterTest.class);
-		suite.addTestSuite(AuthorFilterTest.class);
-		suite.addTestSuite(CategoryFilterTest.class);
-		suite.addTestSuite(DifficultyFilterTest.class);
-	}
-	
-	private static void testPersistence()
-	{
-		suite.addTestSuite(TestStubDB.class);
 	}
 }
